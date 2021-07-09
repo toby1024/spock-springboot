1898ff6e0c17ab3b50bee6dd388ea96916714ab4
zhangbin <zhangbin06@souche.com>
2021-07-09 10:16:11 +0800

    init: project created

---
+++ README.md
@@ -0,0 +1,3 @@
+# spock-springboot
+
+springboot使用spock进行单元测试demo
---
+++ src/main/java/com/example/spockspringboot/SpockSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.spockspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpockSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpockSpringbootApplication.class, args);
+    }
+
+}
